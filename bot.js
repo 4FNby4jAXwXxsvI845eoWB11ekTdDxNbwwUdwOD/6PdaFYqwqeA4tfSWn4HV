f736df9e72dbc3b2da15cc6431ce8ba6bf0b62e8
4FNby4jAXwXxsvI845eoWB11ekTdDxNbwwUdwOD <45238840+4FNby4jAXwXxsvI845eoWB11ekTdDxNbwwUdwOD@users.noreply.github.com>
2018-11-23 13:52:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,18 @@
-﻿const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-client.on('message', message => {
-    if (message.content === '...') {
-    });
+var prefix = "1";
+
+client.on('ready', () => {
+  console.log(`...`);  
+});
+
+client.on('ready', async() => {
+var server = "514822456998101002"; //
+var channel = "514822457706807312";//
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein**')
+    },305);
+ });
+
+ 
 client.login("NTE0ODI0MzQ0NjE1MjU2MDY3.DtcOvQ.RtuKA7Ou6jaQ5NY2H9AWVhYMyUk");
