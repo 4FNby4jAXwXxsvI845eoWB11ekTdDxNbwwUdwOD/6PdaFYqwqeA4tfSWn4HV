be0503c2511220f7e6bebbee18221ad0bdc33474
4FNby4jAXwXxsvI845eoWB11ekTdDxNbwwUdwOD <45238840+4FNby4jAXwXxsvI845eoWB11ekTdDxNbwwUdwOD@users.noreply.github.com>
2018-11-23 13:47:37 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,6 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('message', message => {
+    if (message.content === '...') {
+    });
+client.login("NTE0ODI0MzQ0NjE1MjU2MDY3.DtcOvQ.RtuKA7Ou6jaQ5NY2H9AWVhYMyUk");
